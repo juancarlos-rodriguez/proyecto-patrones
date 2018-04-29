1e5784e1e4f70f1e852b659a0f6365fb0b1e2e2c
Kevin Martinez <kmartineze@ucenfotec.ac.cr.com>
2018-04-28 22:58:44 -0600

    Create Character abstract class

--- src/main/java/com/ucenfotec/personajes/Character.java
+++ src/main/java/com/ucenfotec/personajes/Character.java
@@ -1,85 +1,11 @@
 package com.ucenfotec.personajes;
 
-import java.util.ArrayList;
-import com.ucenfotec.patrones.logic.Item;
-import com.ucenfotec.patrones.logic.Mission;
-
-public class Character implements IAttack, IAbility {
+public abstract class Character {
 	
-	private String name;
-	private int health;
-	private int experience;
-	private int power;
-	private ArrayList<Item> myItems = new ArrayList<>();
-    private ArrayList<Mission> myMissions;
-
-
-    public Character(String pName, int pHealth, int pExperience, int pPower) {
-		this.name = pName;
-		this.health = pHealth;
-		this.experience = pExperience;
-		this.power = pPower;
-	}
+	public void attack(Character enemy) {}
 	
-	public Item getObject() {
-		return null;
-	}
-	
-	public ArrayList<Item> loadObject(Item pItem) {
-		myItems.add(pItem);
-		return myItems;
-	}
-	
-	public int addExperience() {
-		return 0;
-	}
-	
-	public void moveCharacter() {
-	}
-	
+	public void receiveDamage(int pAttack) {
 
-	@Override
-	public String ability() {
-		return null;
 	}
-
-	@Override
-    public void attack(Enemy enemy) {
-        enemy.receiveDamage(this.getPower());
-    }
-
-    public String getName() {
-		return name;
-	}
-
-	public int getHealth() {
-		return health;
-	}
-
-    private void setHealth(int health) {
-        this.health = health;
-    }
-
-	public int getExperience() {
-		return experience;
-	}
-
-	public int getPower() {
-		return power;
-	}
-
-	@Override
-	public String toString() {
-		return "Character [name=" + name + ", health=" + health + ", experience=" + experience + ", power=" + power
-				+ ", myItems=" + myItems + ", myMissions=" + myMissions + "]";
-	}
-    @Override
-    public void receiveDamage(int pAttack) {
-        if ((this.getHealth() - pAttack) > 0) {
-            this.setHealth(this.getHealth() - pAttack);
-        } else {
-            this.setHealth(0);
-        }
-    }
 
 }
